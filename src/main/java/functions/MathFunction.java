79948f5d571b0fb960e913d44b8a5b49eca0140c
Kirill Mikhailov <polupoker06@bk.ru>
2025-11-24 21:11:39 +0400

    all

--- src/main/java/functions/MathFunction.java
+++ src/main/java/functions/MathFunction.java
@@ -2,8 +2,6 @@ package functions;
 
 public interface MathFunction {
     double apply(double x);
-
-    default MathFunction andThen(MathFunction after) {
-        return x -> after.apply(this.apply(x));
+    default MathFunction andThen(MathFunction after) { return x -> after.apply(this.apply(x));
     }
 }
